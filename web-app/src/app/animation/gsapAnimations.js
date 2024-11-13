bbdf102e75f379e8bf2463bf0096b4a79ea20db4
Sonja Farrell <sonjafarrell@gmail.com>
2024-11-12 23:05:45 -0800

    add in animation demo

--- web-app/src/app/animation/gsapAnimations.js
+++ web-app/src/app/animation/gsapAnimations.js
@@ -1,11 +1,24 @@
+import {useRef} from "react";
+import gsap from 'gsap';
+import {useGSAP} from '@gsap/react';
 import styles from "./gsap.module.css";
 
+gsap.registerPlugin(useGSAP);
+
 export default function GSAPAnimations() {
+    const container = useRef();
+
+    useGSAP(() => {
+        gsap.to('.confetti', {rotation: 360, repeat: -1, duration: 3});
+        gsap.to('.heart', {rotation: 360, repeat: -1, duration: 3});
+        gsap.to('.lettuce', {rotation: 360, repeat: -1, duration: 3});
+    },{scope: container});
+
     return (
-        <section className={styles.cssSection}>
-            <span className={styles.confetti}>🎊</span>
-            <span className={styles.heart}>🩵</span>
-            <span className={styles.lettuce}>🥬</span>
+        <section className={styles.cssSection} ref={container}>
+            <span className={styles.confetti + " confetti"}>🎊</span>
+            <span className={styles.heart + " heart"}>🩵</span>
+            <span className={styles.lettuce + " lettuce"}>🥬</span>
         </section>
     );
 }
--- web-app/src/app/animation/waapiAnimations.js
+++ web-app/src/app/animation/waapiAnimations.js
@@ -1,11 +1,25 @@
+import {useRef, useEffect} from "react";
 import styles from "./waapi.module.css";
 
 export default function WAAPIAnimations() {
+    const flowerRef = useRef();
+    const balloonRef = useRef();
+    const starRef = useRef();
+
+    useEffect(() => {
+        flowerRef.current.animate(rotate, rotateTiming);
+        balloonRef.current.animate(rotate, rotateTiming);
+        starRef.current.animate(rotate, rotateTiming);
+    })
+
+    const rotate = [{transform: "rotate(0)"},{transform: "rotate(360deg)"}];
+    const rotateTiming = {duration: 3000, iterations: Infinity};
+
 return (
         <section className={styles.cssSection}>
-            <span className={styles.flower}>🌸</span>
-            <span className={styles.balloon}>🎈</span>
-            <span className={styles.star}>⭐️</span>
+            <span className={styles.flower} ref={flowerRef}>🌸</span>
+            <span className={styles.balloon} ref={balloonRef}>🎈</span>
+            <span className={styles.star} ref={starRef}>⭐️</span>
         </section>
     );
 }
